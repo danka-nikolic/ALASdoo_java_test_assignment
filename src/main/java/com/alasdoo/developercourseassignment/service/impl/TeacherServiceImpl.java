6e3842f350551097b5dea7fde37519fbc4b34fad
DANKA-PC\Danka <dankaninkolic90@gmail.com>
2020-12-23 20:42:26 +0100

    [fix-backend] :: TeacherServiceImpl -  Implement save method

--- src/main/java/com/alasdoo/developercourseassignment/service/impl/TeacherServiceImpl.java
+++ src/main/java/com/alasdoo/developercourseassignment/service/impl/TeacherServiceImpl.java
@@ -1,44 +1,55 @@
 package com.alasdoo.developercourseassignment.service.impl;
 
-import com.alasdoo.developercourseassignment.dto.TeacherDTO;
-import com.alasdoo.developercourseassignment.service.TeacherService;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.alasdoo.developercourseassignment.dto.TeacherDTO;
+import com.alasdoo.developercourseassignment.entity.Teacher;
+import com.alasdoo.developercourseassignment.mapper.TeacherMapper;
+import com.alasdoo.developercourseassignment.repository.TeacherRepository;
+import com.alasdoo.developercourseassignment.service.TeacherService;
 
 @Service
 public class TeacherServiceImpl implements TeacherService {
 
-	@Override
-    public TeacherDTO findOne(Integer id) {
-        return null;
-    }
+	@Autowired
+	private TeacherRepository teacherRepository;
+
+	@Autowired
+	private TeacherMapper teacherMapper;
 
 	@Override
-    public List<TeacherDTO> findAll() {
-        return null;
-    }
+	public TeacherDTO findOne(Integer id) {
+		return null;
+	}
 
 	@Override
-    public TeacherDTO save(TeacherDTO teacherDTO) {
-        return null;
-    }
+	public List<TeacherDTO> findAll() {
+		return null;
+	}
 
 	@Override
-    public void remove(Integer id) throws IllegalArgumentException {
-    }
+	public TeacherDTO save(TeacherDTO teacherDTO) {
+		Teacher teacher = teacherMapper.transformToEntity(teacherDTO);
+        return teacherMapper.transformToDTO(teacherRepository.save(teacher));
+	}
 
 	@Override
-    public TeacherDTO update(Integer id, TeacherDTO teacherDTO) {
-        return null;
-    }
+	public void remove(Integer id) throws IllegalArgumentException {
+	}
 
-    public TeacherDTO findByTeacherNameAndTeacherSurname(String name, String surname) {
-        return null;
-    }
+	@Override
+	public TeacherDTO update(Integer id, TeacherDTO teacherDTO) {
+		return null;
+	}
 
-    public TeacherDTO findByTeacherEmail(String email) {
-        return null;
-    }
+	public TeacherDTO findByTeacherNameAndTeacherSurname(String name, String surname) {
+		return null;
+	}
+
+	public TeacherDTO findByTeacherEmail(String email) {
+		return null;
+	}
 }
