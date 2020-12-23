9fda65ec3399469395a7cafe9af040a044e8ad94
DANKA-PC\Danka <dankaninkolic90@gmail.com>
2020-12-23 20:31:02 +0100

    [fix-backend] :: Override methods in TeacherServiceImpl

--- src/main/java/com/alasdoo/developercourseassignment/service/impl/TeacherServiceImpl.java
+++ src/main/java/com/alasdoo/developercourseassignment/service/impl/TeacherServiceImpl.java
@@ -1,28 +1,35 @@
 package com.alasdoo.developercourseassignment.service.impl;
 
 import com.alasdoo.developercourseassignment.dto.TeacherDTO;
+import com.alasdoo.developercourseassignment.service.TeacherService;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class TeacherServiceImpl {
+public class TeacherServiceImpl implements TeacherService {
 
+	@Override
     public TeacherDTO findOne(Integer id) {
         return null;
     }
 
+	@Override
     public List<TeacherDTO> findAll() {
         return null;
     }
 
+	@Override
     public TeacherDTO save(TeacherDTO teacherDTO) {
         return null;
     }
 
+	@Override
     public void remove(Integer id) throws IllegalArgumentException {
     }
 
+	@Override
     public TeacherDTO update(Integer id, TeacherDTO teacherDTO) {
         return null;
     }
