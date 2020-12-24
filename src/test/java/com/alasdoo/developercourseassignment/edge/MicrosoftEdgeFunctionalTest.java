55e630f2c657e17b72d1c2145b4cd5b3cc464783
DANKA-PC\Danka <dankaninkolic90@gmail.com>
2020-12-24 23:38:50 +0100

    [selenium] ::  MicrosoftEdgeFunctionalTest - Create first selenium test which opens Microsoft Edge browser and navigates to React application settings page

---
+++ src/test/java/com/alasdoo/developercourseassignment/edge/MicrosoftEdgeFunctionalTest.java
@@ -0,0 +1,43 @@
+package com.alasdoo.developercourseassignment.edge;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class MicrosoftEdgeFunctionalTest {
+	protected static WebDriver driver;
+
+	@BeforeAll
+	public static void setUp() {
+		System.setProperty("webdriver.edge.driver", "src/test/resources/msedgedriver.exe");
+		driver = new EdgeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+	@AfterEach
+	public void cleanUp() {
+		driver.manage().deleteAllCookies();
+	}
+
+	@AfterAll
+	public static void tearDown() {
+		driver.close();
+	}
+	
+	@Test
+	public void firefoxDemoTest() {
+		driver.get("http://localhost:3000/settings");
+		
+		try {
+			Thread.sleep(7000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
