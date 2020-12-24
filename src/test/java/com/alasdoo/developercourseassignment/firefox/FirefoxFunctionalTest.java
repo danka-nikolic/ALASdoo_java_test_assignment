ad8871074afccf0fd9803f3f3f5ee46240b59ed6
DANKA-PC\Danka <dankaninkolic90@gmail.com>
2020-12-24 22:50:29 +0100

    [selenium] :: FirefoxFunctionalTest - Create first successfull selenium test which opens Firefox browser and navigates to React application settings page

--- src/test/java/com/alasdoo/developercourseassignment/firefox/FirefoxFunctionalTest.java
+++ src/test/java/com/alasdoo/developercourseassignment/firefox/FirefoxFunctionalTest.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -26,6 +27,17 @@ public class FirefoxFunctionalTest {
 	@AfterAll
 	public static void tearDown() {
 		driver.close();
+	}
+	
+	@Test
+	public void firefoxDemoTest() {
+		driver.get("http://localhost:3000/settings");
+		
+		try {
+			Thread.sleep(7000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
