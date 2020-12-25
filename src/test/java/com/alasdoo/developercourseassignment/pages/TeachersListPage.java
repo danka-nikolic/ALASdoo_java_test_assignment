0f0b4b139dee0cfafff560cb95855b341099032d
DANKA-PC\Danka <dankaninkolic90@gmail.com>
2020-12-25 18:23:03 +0100

    [selenium] :: CreateTeachersListPage - Add logic for reading teachers from table

---
+++ src/test/java/com/alasdoo/developercourseassignment/pages/TeachersListPage.java
@@ -0,0 +1,52 @@
+package com.alasdoo.developercourseassignment.pages;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.alasdoo.developercourseassignment.dto.TeacherDTO;
+
+public class TeachersListPage extends PageObject {
+	
+	@FindBy(className = "MuiDataGrid-colCellTitle")
+    private List<WebElement> headerElements;
+
+	@FindBy(xpath = "//*[@id=\"root\"]/div/main/div[2]/div/div/div[1]/div/div[2]/div[2]/div/div/div")
+	private WebElement tableContent;
+
+	public TeachersListPage(WebDriver driver) {
+		super(driver);
+	}
+
+	public List<WebElement> getHeaderElements() {
+		return headerElements;
+	}
+
+	public WebElement getTableContent() {
+		return tableContent;
+	}
+	
+	public List<TeacherDTO> readTeachersFromTable() {
+		List<TeacherDTO> teachers = new ArrayList<>();
+
+		String allTableContent = tableContent.getText();
+		List<String> tableParts = Arrays.asList(allTableContent.split("\r?\n"));
+		
+		for (int i=0; i<tableParts.size(); i=i+3) {
+			TeacherDTO teacherDTO = new TeacherDTO();
+
+			teacherDTO.setId(Integer.valueOf(tableParts.get(i)));
+			teacherDTO.setTeacherName(tableParts.get(i+1));
+			teacherDTO.setTeacherSurname(tableParts.get(i+2));
+
+			teachers.add(teacherDTO);
+		}
+		
+		return teachers;
+	}
+
+}
