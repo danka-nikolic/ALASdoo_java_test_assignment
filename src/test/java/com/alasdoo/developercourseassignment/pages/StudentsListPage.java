12e77220f3e574afa543158cbfa357e83d5aa1c2
DANKA-PC\Danka <dankaninkolic90@gmail.com>
2020-12-25 17:52:47 +0100

    [selenium] :: Create StudentsListPage - Add logic for reading students from table

--- src/test/java/com/alasdoo/developercourseassignment/pages/StudentsListPage.java
+++ src/test/java/com/alasdoo/developercourseassignment/pages/StudentsListPage.java
@@ -1,15 +1,22 @@
 package com.alasdoo.developercourseassignment.pages;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import com.alasdoo.developercourseassignment.dto.StudentDTO;
+
 public class StudentsListPage extends PageObject {
 	
 	@FindBy(className = "MuiDataGrid-colCellTitle")
     private List<WebElement> headerElements;
+
+	@FindBy(xpath = "//*[@id=\"root\"]/div/main/div[2]/div[1]/div/div[1]/div/div[2]/div[2]/div/div/div")
+	private WebElement tableContent;
 
 	public StudentsListPage(WebDriver driver) {
 		super(driver);
@@ -19,8 +26,29 @@ public class StudentsListPage extends PageObject {
 		return headerElements;
 	}
 
-	public void setHeaderElements(List<WebElement> headerElements) {
-		this.headerElements = headerElements;
+	public WebElement getTableContent() {
+		return tableContent;
+	}
+	
+	public List<StudentDTO> readStudentsFromTable() {
+		List<StudentDTO> students = new ArrayList<>();
+
+		String allTableContent = tableContent.getText();
+		List<String> tableParts = Arrays.asList(allTableContent.split("\r?\n"));
+		
+		for (int i=0; i<tableParts.size(); i=i+5) {
+			StudentDTO studentDTO = new StudentDTO();
+
+			studentDTO.setId(Integer.valueOf(tableParts.get(i)));
+			studentDTO.setName(tableParts.get(i+1));
+			studentDTO.setSurname(tableParts.get(i+2));
+			studentDTO.setAccountName(tableParts.get(i+3));
+			studentDTO.setEmail(tableParts.get(i+4));
+
+			students.add(studentDTO);
+		}
+		
+		return students;
 	}
 
 }
