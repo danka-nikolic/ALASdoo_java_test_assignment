8ae7e513007e74a0280716eb996545a7d3813c31
DANKA-PC\Danka <dankaninkolic90@gmail.com>
2020-12-25 19:44:07 +0100

    [selenium] :: Create CoursesListPageTest - test number of couses and some courses data

---
+++ src/test/java/com/alasdoo/developercourseassignment/tests/CoursesListPageTest.java
@@ -0,0 +1,45 @@
+package com.alasdoo.developercourseassignment.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+
+import com.alasdoo.developercourseassignment.dto.DeveloperCourseDTO;
+import com.alasdoo.developercourseassignment.pages.CoursesListPage;
+
+public class CoursesListPageTest extends FunctionalTest {
+	
+	@Test
+	public void checkHeaderNames() {
+		driver.get("http://localhost:3000/course");
+
+		CoursesListPage courseListPage = new CoursesListPage(driver);
+	
+		List<WebElement> headerElements = courseListPage.getHeaderElements();
+
+		assertEquals("ID", headerElements.get(0).getText());
+		assertEquals("Course name", headerElements.get(1).getText());
+		assertEquals("Cost per class", headerElements.get(2).getText());
+		assertEquals("Classes per week", headerElements.get(3).getText());
+	}
+
+	@Test
+	public void checkTableElements() {
+		driver.get("http://localhost:3000/course");
+
+		CoursesListPage courseListPage = new CoursesListPage(driver);
+
+		List<DeveloperCourseDTO> courses = courseListPage.readCoursesFromTable();
+
+		int numberOfCoursesOnPage = 10;
+		assertEquals(numberOfCoursesOnPage, courses.size());
+
+		assertEquals("Mauris eu turpis.", courses.get(0).getDeveloperCourseName());
+
+		assertEquals(75, courses.get(2).getCostPerClass());
+	}
+
+}
