257a6e3ab685b73be09bb79e1c0d834315be6cd7
DANKA-PC\Danka <dankaninkolic90@gmail.com>
2020-12-25 18:45:39 +0100

    [selenium] :: Create TeacherListPageTest - test number of teachers and some teachers data

---
+++ src/test/java/com/alasdoo/developercourseassignment/tests/TeacherListPageTest.java
@@ -0,0 +1,44 @@
+package com.alasdoo.developercourseassignment.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+
+import com.alasdoo.developercourseassignment.dto.TeacherDTO;
+import com.alasdoo.developercourseassignment.pages.TeachersListPage;
+
+public class TeacherListPageTest extends FunctionalTest {
+	
+	@Test
+	public void checkHeaderNames() {
+		driver.get("http://localhost:3000/teacher");
+
+		TeachersListPage teacherListPage = new TeachersListPage(driver);
+		
+		List<WebElement> headerElements = teacherListPage.getHeaderElements();
+
+		assertEquals("ID", headerElements.get(0).getText());
+		assertEquals("Name", headerElements.get(1).getText());
+		assertEquals("Surname", headerElements.get(2).getText());
+	}
+
+	@Test
+	public void checkTableElements() {
+		driver.get("http://localhost:3000/teacher");
+
+		TeachersListPage teacherListPage = new TeachersListPage(driver);
+		
+		List<TeacherDTO> teachers = teacherListPage.readTeachersFromTable();
+
+		int numberOfTeachersOnPage = 10;
+		assertEquals(numberOfTeachersOnPage, teachers.size());
+		
+		assertEquals("Linda", teachers.get(3).getTeacherName());
+
+		assertEquals("Daniels", teachers.get(8).getTeacherSurname());
+	}
+
+}
