be739f42ceba26fea9fdfcba5ce4e138d0cc242c
DANKA-PC\Danka <dankaninkolic90@gmail.com>
2020-12-25 22:33:51 +0100

    [selenium] :: Implement capture screenshot on tests failure

--- src/test/java/com/alasdoo/developercourseassignment/tests/FunctionalTest.java
+++ src/test/java/com/alasdoo/developercourseassignment/tests/FunctionalTest.java
@@ -1,21 +1,30 @@
 package com.alasdoo.developercourseassignment.tests;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestWatcher;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
-public class FunctionalTest {
+@ExtendWith(FunctionalTest.class)
+public class FunctionalTest implements TestWatcher {
 
 	protected static WebDriver driver;
 
@@ -34,6 +43,12 @@ public class FunctionalTest {
 	public static void tearDown() {
 		driver.close();
 	}
+	
+	@Override
+    public void testFailed(ExtensionContext extensionContext, Throwable throwable) {
+		String methodName = extensionContext.getDisplayName().substring(0, extensionContext.getDisplayName().length() - 2);
+        captureScreenshot(methodName);
+    }
 
 	private static void setupBrowser() throws IOException {
 		String browser = System.getProperty("BROWSER_TYPE");
@@ -82,4 +97,19 @@ public class FunctionalTest {
 		System.setProperty("webdriver.edge.driver", "src/test/resources/msedgedriver.exe");
 		driver = new EdgeDriver();
 	}
+	
+	private void captureScreenshot(String fileName) {
+        try {
+        	LocalTime time = LocalTime.now();
+        	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("_HH_mm_ss");
+        	String sTime = time.format(dtf);
+            new File("src/test/resources/screenshots/").mkdirs(); // Insure directory is there
+            FileOutputStream out = new FileOutputStream("src/test/resources/screenshots/" + fileName + sTime + ".png");
+            out.write(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES));
+            out.close();
+        } catch (Exception e) {
+        	// TODO - add logger
+            e.printStackTrace();
+        }
+    }
 }
